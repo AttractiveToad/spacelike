6f9e8247cbcf7fe1019566e9145a398184e46e43
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2024-07-12 19:20:08 +0200

    Update code

--- scripts/Killzone.cs
+++ scripts/Killzone.cs
@@ -3,15 +3,9 @@ using System;
 
 public partial class Killzone : Area2D
 {
-	public override void _Ready()
-	{
-		// get note timer
-		Timer myTimer = GetNode<Timer>("Timer");
-	}
-	
 	private void _on_body_entered(Node2D body)
 	{
-		Godot.GD.Print("You died!")
+		Godot.GD.Print("You died!");
 	}
 }
 
